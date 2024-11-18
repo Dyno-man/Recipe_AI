2ce6ea3aa7e7d55fc382620dfcb282cbed156177
CollinT123 <collinwtucker@gmail.com>
2024-11-18 13:22:09 -0500

    VoiceToTextV1
    Added very basic voice to text with it's own textbox

--- recipe-ai/src/App.js
+++ recipe-ai/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import SearchBar from './SearchBar';
 import logo1 from './images/recipeAiLogo.webp'
+import VoiceToText from "./voiceToText";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           Learn React
         </a> */}
         <SearchBar/>
+          <VoiceToText/>
       </header>
     </div>
   );
